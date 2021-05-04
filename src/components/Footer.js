7a716e10b5b3832da936b5448a2eb421b9cdec3c
tlylt <liu_yongliang@hotmail.com>
2021-05-04 10:38:51 +0800

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
-        <footer className="py-5 hidden">
-            {/* <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/tlylt/from-the-future" /> */}
+        <footer className="py-5 bg-sku-light justify-center">
+            <small>Have an awesome enhancement idea ? Contribute & Like on <a className="underline" href="https://github.com/tlylt/from-the-future" rel="noopener noreferrer" target="_blank" aria-label="link to github repository">Github</a></small>
+            <img className="mx-auto hidden" alt="GitHub Repo stars" src="https://img.shields.io/github/stars/tlylt/from-the-future" />
         </footer>
     )
 }
